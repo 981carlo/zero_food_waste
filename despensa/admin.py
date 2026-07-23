d0ec3b0ec45bdc4422ec0a43ba2c758943b9decb
Carlo <81carlo@gmail.com>
2026-07-23 20:11:06 +0200

    feat: registrar modelo Alimento en Django Admin

--- despensa/admin.py
+++ despensa/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Alimento
+
+
+@admin.register(Alimento)
+class AlimentoAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre",
+        "cantidad",
+        "unidad_medida",
+        "fecha_caducidad_formateada",
+        "usuario",
+    )
+    search_fields = ("nombre", "usuario__username")
+    list_filter = ("unidad_medida", "fecha_caducidad")
+    ordering = ("fecha_caducidad",)
+
+    @admin.display(
+        ordering="fecha_caducidad",
+        description="Fecha de caducidad",
+    )
+    def fecha_caducidad_formateada(self, obj):
+        return obj.fecha_caducidad.strftime("%d/%m/%Y")
