18be5d65f496f2c626daef5ea77b17a94a668104
Carlo <81carlo@gmail.com>
2026-08-24 12:10:14 +0200

    style: mejorar estilos

--- despensa/forms.py
+++ despensa/forms.py
@@ -13,6 +13,26 @@ class AlimentoForm(forms.ModelForm):
         input_formats=["%Y-%m-%d"],
     )
     
+    unidad_medida = forms.ChoiceField(
+        choices=[("", "Selecciona una unidad")] + list(Alimento.UnidadMedida.choices),
+        label="Unidad de medida",
+        required=True,
+    )
+    
+    nombre = forms.CharField(
+        max_length=50,
+        label="Nombre",
+        error_messages={
+            "required": "Este campo es requerido.",
+            "max_length": "El nombre no puede tener más de 50 caracteres.",
+        },
+    )
+    
+    def clean_nombre(self):
+        nombre = self.cleaned_data["nombre"].strip()
+
+        return nombre[:1].upper() + nombre[1:]
+    
     class Meta:
         model = Alimento
         fields = [
